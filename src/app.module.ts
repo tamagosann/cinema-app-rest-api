6bad582edf892f68c528e5e7d05ab5aeef3da54f
tamagosann <hssvs619@gmail.com>
2022-01-23 15:40:18 +0900

    added typeOrm

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,22 @@ import { SsrModule } from './ssr/ssr.module';
 import { FilmController } from './film/film.controller';
 import { FilmService } from './film/film.service';
 import { FilmModule } from './film/film.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [SsrModule, FilmModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'mysql',
+      port: 3306,
+      username: 'admin',
+      password: 'pass',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    SsrModule,
+    FilmModule,
+  ],
   controllers: [AppController, SsrController, FilmController],
   providers: [AppService, SsrService, FilmService],
 })
